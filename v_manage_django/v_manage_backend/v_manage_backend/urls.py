fd8a36f35d9afb816581973aba6e38a1e9c5d4d8
Jay Jain <jay4codes@gmail.com>
2022-10-05 03:55:54 +0530

    merge fixes

--- v_manage_django/v_manage_backend/v_manage_backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from core.views import front
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", front, name="front"),
-]
