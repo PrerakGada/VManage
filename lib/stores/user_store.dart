f0372444cba737cec7cdef93691d5f10744ba9d7
Jay Jain <jay4codes@gmail.com>
2022-10-04 19:39:57 +0530

    initial commit

--- lib/stores/user_store.dart
+++
@@ -1,9 +0,0 @@
-import 'state_keeper.dart';
-
-class UserStore extends StateKeeper {
-  UserStore._();
-
-  static final UserStore _instance = UserStore._();
-
-  factory UserStore() => _instance;
-}
