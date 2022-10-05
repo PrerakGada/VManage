b3a60d7f7786a1f1f2e3f00efab2eb93aa1237e1
Prerak Gada <prerakgada07@gmail.com>
2022-10-05 06:51:26 +0530

    Google Maps integration with iOS

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -11,32 +11,42 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final NavigationService _navigationService =
-        get_it_instance_const<NavigationService>();
+    get_it_instance_const<NavigationService>();
     return Scaffold(
       appBar: AppBar(
-        leadingWidth: 150,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Hi Prerak!',
-                style: TextStyle(fontSize: 20),
+          flexibleSpace: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hi Prerak!',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(height: 8),
+                      GestureDetector(
+                        onTap: () {
+                          _navigationService.navigateTo(RoutePath.EditProfile);
+                        },
+                        child: Text(
+                          'Edit Profile  >',
+                          style: TextStyle(
+                              fontSize: 12, color: AppColors.greyLight),
+                        ),
+                      ),
+                    ],
+                  ),
+                  CircleAvatar(backgroundColor: AppColors.black, radius: 24),
+                ],
               ),
-              SizedBox(height: 8),
-              GestureDetector(
-                onTap: () {
-                  _navigationService.navigateTo(RoutePath.EditProfile);
-                },
-                child: Text(
-                  'Edit Profile  >',
-                  style: TextStyle(fontSize: 12, color: AppColors.greyLight),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+
       ),
       body: Center(
         child: Text('Hello'),
--- lib/widgets/EventsMap.dart
+++ lib/widgets/EventsMap.dart
@@ -1,18 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class EventsMap extends StatelessWidget {
+class EventsMap extends StatefulWidget {
   const EventsMap({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<EventsMap> createState() => _EventsMapState();
+}
+
+class _EventsMapState extends State<EventsMap> {
+  // DataService? d1;
+  GoogleMapController? mapController;
+
+  void initstate() {
+    super.initState();
+
+    // ignore: prefer_collection_literals
+    markers = Set.from([]);
+  }
+
+  void _onmapcreated(GoogleMapController controller) {
+    setState(() {
+      markers.add(
+        Marker(
+            infoWindow: InfoWindow(
+              title: 'Hello world',
+              snippet: 'Basic',
+            ),
+            markerId: MarkerId('id-1'),
+            position: LatLng(19.0760, 72.8777)),
+      );
+    });
+  }
+
+  //final Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> markers = {};
+
+  BitmapDescriptor? customIcon1;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    // ignore: prefer_const_constructors
+    target: LatLng(19.0760, 72.8777),
+    zoom: 14.4746,
+  );
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blue,
-      width: MediaQuery.of(context).size.width *0.9,
+      width: MediaQuery.of(context).size.width * 0.9,
       height: 400,
       //@jay write your code here:
-      //child: ,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 0,
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                markers.add(
+                  Marker(
+                    icon: BitmapDescriptor.defaultMarkerWithHue(
+                        BitmapDescriptor.hueGreen),
+                    infoWindow: InfoWindow(
+                      title: 'contractor',
+                      snippet: 'name',
+                    ),
+                    markerId: MarkerId('$index'),
+                    position: LatLng(
+                      double.parse('19.0760'),
+                      double.parse('72.8777'),
+                    ),
+                  ),
+                );
+                return Container();
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: GoogleMap(
+                onMapCreated: _onmapcreated,
+                initialCameraPosition: _kGooglePlex,
+                markers: markers,
+              ),
+              height: 600,
+              width: 400,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
