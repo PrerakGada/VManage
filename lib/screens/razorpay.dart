03f9d425e18b33b8c28563c9132f65e8c7167100
Jay Jain <jay4codes@gmail.com>
2022-10-05 17:52:31 +0530

    razorpay added

--- lib/screens/razorpay.dart
+++ lib/screens/razorpay.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
 
@@ -5,71 +6,84 @@ class Payment extends StatefulWidget {
   const Payment({Key? key}) : super(key: key);
 
   @override
-  State<Payment> createState() => _PaymentState();
+  _PaymentState createState() => _PaymentState();
 }
 
 class _PaymentState extends State<Payment> {
-  Razorpay _razorpay = Razorpay();
+  late var _razorpay;
+  var amountController = TextEditingController();
 
   @override
   void initState() {
-    super.initState();
     _razorpay = Razorpay();
     _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
     _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
     _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _razorpay.clear();
-  }
-
-  void openCheckout() async {
-    var options = {
-      'key': 'rzp_live_ILgsfZCZoFIKMb',
-      'amount': 100,
-      'name': 'Acme Corp.',
-      'description': 'Fine T-Shirt',
-      'retry': {'enabled': true, 'max_count': 1},
-      'send_sms_hash': true,
-      'prefill': {'contact': '8888888888', 'email': 'test@razorpay.com'},
-      'external': {
-        'wallets': ['paytm']
-      }
-    };
-
-    try {
-      _razorpay.open(options);
-    } catch (e) {
-      debugPrint('Error: e');
-    }
+    super.initState();
   }
 
   void _handlePaymentSuccess(PaymentSuccessResponse response) {
-    print('Success Response: $response');
-    /*Fluttertoast.showToast(
-        msg: "SUCCESS: " + response.paymentId!,
-        toastLength: Toast.LENGTH_SHORT); */
+    // Do something when payment succeeds
+    print("Payment Done $response");
   }
 
   void _handlePaymentError(PaymentFailureResponse response) {
-    print('Error Response: $response');
-    /* Fluttertoast.showToast(
-        msg: "ERROR: " + response.code.toString() + " - " + response.message!,
-        toastLength: Toast.LENGTH_SHORT); */
+    // Do something when payment fails
+    print("Payment Fail $response");
   }
 
   void _handleExternalWallet(ExternalWalletResponse response) {
-    print('External SDK Response: $response');
-    /* Fluttertoast.showToast(
-        msg: "EXTERNAL_WALLET: " + response.walletName!,
-        toastLength: Toast.LENGTH_SHORT); */
+    // Do something when an external wallet is selected
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: TextField(
+                controller: amountController,
+                decoration:
+                    const InputDecoration(hintText: "Enter your Amount"),
+              ),
+            ),
+            MaterialButton(
+                color: Colors.grey,
+                child: Text("Pay Amount"),
+                onPressed: () {
+                  ///Make payment
+                  var options = {
+                    'key': "rzp_test_hZPnC0MBwmXbjh",
+                    // amount will be multiple of 100
+                    'amount': (int.parse(amountController.text) * 100)
+                        .toString(), //So its pay 500
+                    'name': 'VManage',
+                    'description': 'Demo',
+                    'timeout': 300, // in seconds
+                    'prefill': {
+                      'contact': '8787878787',
+                      'email': 'jay4emails@gmail.com'
+                    }
+                  };
+                  _razorpay.open(options);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _razorpay.clear();
+    super.dispose();
   }
 }
