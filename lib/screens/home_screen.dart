9d24718fa83eaf480ef5903d86c02e37e18f5cc8
Prerak Gada <prerakgada07@gmail.com>
2022-10-05 13:24:40 +0530

    Added Event Card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:eos_hackover3/Theme/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -22,72 +21,189 @@ class HomeScreen extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                (kIsWeb)
-                    ? Wrap(
-                        children: [
-                          Container(
-                            // color: AppColors.black,
-                            width: 150,
-                            height: 150,
-                            child: SfCircularChart(
-                              series: [
-                                RadialBarSeries<ChartData, String>(
-                                    dataSource: chartData,
-                                    xValueMapper: (ChartData data, _) => data.x,
-                                    yValueMapper: (ChartData data, _) =>
-                                        data.y),
+                EventCard(
+                  imageUrl:
+                      'https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+                  title: 'HackOver 3.0',
+                  date: '05/10/2022',
+                  description: "Navratri is the \n wecwaecs erv..",
+                  price: '\$29',
+                ),
+                EventCard(
+                  imageUrl:
+                  'https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+                  title: 'HackOver 3.0',
+                  date: '05/10/2022',
+                  description: "Navratri is the \n wecwaecs erv..",
+                  price: '\$29',
+                ),
+                EventCard(
+                  imageUrl:
+                  'https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+                  title: 'HackOver 3.0',
+                  date: '05/10/2022',
+                  description: "Navratri is the \n wecwaecs erv..",
+                  price: '\$29',
+                ),
+                EventCard(
+                  imageUrl:
+                  'https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+                  title: 'HackOver 3.0',
+                  date: '05/10/2022',
+                  description: "Navratri is the \n wecwaecs erv..",
+                  price: '\$29',
+                ),
+
+                // (kIsWeb)
+                //     ? Wrap(
+                //         children: [
+                //           Container(
+                //             // color: AppColors.black,
+                //             width: 150,
+                //             height: 150,
+                //             child: SfCircularChart(
+                //               series: [
+                //                 RadialBarSeries<ChartData, String>(
+                //                     dataSource: chartData,
+                //                     xValueMapper: (ChartData data, _) => data.x,
+                //                     yValueMapper: (ChartData data, _) =>
+                //                         data.y),
+                //               ],
+                //             ),
+                //           ),
+                //           Container(
+                //             // color: AppColors.black,
+                //             width: 150,
+                //             height: 150,
+                //             child: SfCircularChart(
+                //               series: [
+                //                 RadialBarSeries<ChartData, String>(
+                //                     dataSource: chartData,
+                //                     xValueMapper: (ChartData data, _) => data.x,
+                //                     yValueMapper: (ChartData data, _) =>
+                //                         data.y),
+                //               ],
+                //             ),
+                //           ),
+                //         ],
+                //       )
+                //     : Row(
+                //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //         children: [
+                //           Container(
+                //             // color: AppColors.black,
+                //             width: 150,
+                //             height: 150,
+                //             child: SfCircularChart(
+                //               series: [
+                //                 RadialBarSeries<ChartData, String>(
+                //                     dataSource: chartData,
+                //                     xValueMapper: (ChartData data, _) => data.x,
+                //                     yValueMapper: (ChartData data, _) =>
+                //                         data.y),
+                //               ],
+                //             ),
+                //           ),
+                //           Container(
+                //             // color: AppColors.black,
+                //             width: 150,
+                //             height: 150,
+                //             child: SfCircularChart(
+                //               series: [
+                //                 RadialBarSeries<ChartData, String>(
+                //                     dataSource: chartData,
+                //                     xValueMapper: (ChartData data, _) => data.x,
+                //                     yValueMapper: (ChartData data, _) =>
+                //                         data.y),
+                //               ],
+                //             ),
+                //           ),
+                //         ],
+                //       ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EventCard extends StatelessWidget {
+  const EventCard({
+    Key? key,
+    required this.imageUrl,
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.price,
+  }) : super(key: key);
+
+  final String imageUrl;
+  final String title;
+  final String description;
+  final String date;
+  final String price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        clipBehavior: Clip.antiAlias,
+        child: GestureDetector(
+          child: Container(
+            color: AppColors.black,
+            // padding: EdgeInsets.all(8),
+            width: 400,
+            height: 120,
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.network(
+                    // width: 100,
+                    height: 120,
+                    imageUrl),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              children: [
+                                Text(title),
+                                Text(description,
+                                    overflow: TextOverflow.ellipsis),
                               ],
                             ),
-                          ),
-                          Container(
-                            // color: AppColors.black,
-                            width: 150,
-                            height: 150,
-                            child: SfCircularChart(
-                              series: [
-                                RadialBarSeries<ChartData, String>(
-                                    dataSource: chartData,
-                                    xValueMapper: (ChartData data, _) => data.x,
-                                    yValueMapper: (ChartData data, _) =>
-                                        data.y),
+                            Column(
+                              children: [
+                                Text(date),
+                                Text(price),
                               ],
                             ),
+                          ],
+                        ),
+                        MaterialButton(
+                          minWidth: 100,
+                          height: 30,
+                          color: AppColors.primaryAccent,
+                          padding: EdgeInsets.all(2),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        ],
-                      )
-                    : Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            // color: AppColors.black,
-                            width: 150,
-                            height: 150,
-                            child: SfCircularChart(
-                              series: [
-                                RadialBarSeries<ChartData, String>(
-                                    dataSource: chartData,
-                                    xValueMapper: (ChartData data, _) => data.x,
-                                    yValueMapper: (ChartData data, _) =>
-                                        data.y),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            // color: AppColors.black,
-                            width: 150,
-                            height: 150,
-                            child: SfCircularChart(
-                              series: [
-                                RadialBarSeries<ChartData, String>(
-                                    dataSource: chartData,
-                                    xValueMapper: (ChartData data, _) => data.x,
-                                    yValueMapper: (ChartData data, _) =>
-                                        data.y),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                          onPressed: () {},
+                          child: Text('Register'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
